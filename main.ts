d20d8c6745aef8aa3e16da6ed01e951acfd6c3c8
Tomas Hernandez <t.hernandez11@uniandes.edu.co>
2025-04-13 14:13:48 -0500

    Se bloqueaban las peticiones para conseguir las imagenes de lugares externos, se descargaron dentro del repositorio y las url fueron modificadas para servir

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { Serie } from './serie.js';
 
 var seriesTbody = document.getElementById('series')!;
 var seasonMeanElm = document.getElementById("promedio")!;
+var seriesDetail = document.getElementById("detalle")!; 
 
 renderCoursesInTable(series);
 seasonMeanElm.innerHTML = "Seasons average: ".concat(getSeasonMean(series).toString()); //Cuando accedes a la propiedad innerHTML de un elemento HTML, obtienes o puedes establecer el contenido HTML que está dentro de ese elemento, incluidas las etiquetas HTML y su contenido.
@@ -15,6 +16,11 @@ function renderCoursesInTable(series: Serie[]) {
                                <td><a  href="${serie.url}" target="_blank">${serie.nombre}<a></td>
                                <td>${serie.canal}</td>
                                <td>${serie.temporadas.toString()}</td>`;
+
+        trElement.addEventListener("click", () => {
+            showSeriesDetail(serie);
+        });
+
         seriesTbody.appendChild(trElement);
     });
 }
@@ -24,4 +30,17 @@ function getSeasonMean(series: Serie[]) {
     var promedio = 0;
     series.forEach(function (serie) { return promedio = promedio + serie.temporadas; });
     return promedio/series.length;
+}
+
+function showSeriesDetail(serie: Serie) {
+    console.log('Cargando informacion');
+    seriesDetail.innerHTML = `
+                            <div class="card" style="width: 18rem;">
+                                <img src="${serie.urlImagen}" class="card-img-top" alt="${serie.nombre}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${serie.nombre}</h5>
+                                    <p class="card-text">${serie.descripcion}</p>
+                                    <a href="${serie.url}" target="_blank">${serie.url}</a>
+                                </div>
+                            </div>`;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { series } from './data.js';
 var seriesTbody = document.getElementById('series');
 var seasonMeanElm = document.getElementById("promedio");
+var seriesDetail = document.getElementById("detalle");
 renderCoursesInTable(series);
 seasonMeanElm.innerHTML = "Seasons average: ".concat(getSeasonMean(series).toString()); //Cuando accedes a la propiedad innerHTML de un elemento HTML, obtienes o puedes establecer el contenido HTML que está dentro de ese elemento, incluidas las etiquetas HTML y su contenido.
 function renderCoursesInTable(series) {
@@ -8,6 +9,9 @@ function renderCoursesInTable(series) {
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
         trElement.innerHTML = "<td>".concat(serie.posicion, "</td>\n                               <td><a  href=\"").concat(serie.url, "\" target=\"_blank\">").concat(serie.nombre, "<a></td>\n                               <td>").concat(serie.canal, "</td>\n                               <td>").concat(serie.temporadas.toString(), "</td>");
+        trElement.addEventListener("click", function () {
+            showSeriesDetail(serie);
+        });
         seriesTbody.appendChild(trElement);
     });
 }
@@ -15,4 +19,8 @@ function getSeasonMean(series) {
     var promedio = 0;
     series.forEach(function (serie) { return promedio = promedio + serie.temporadas; });
     return promedio / series.length;
+}
+function showSeriesDetail(serie) {
+    console.log('Cargando informacion');
+    seriesDetail.innerHTML = "\n                            <div class=\"card\" style=\"width: 18rem;\">\n                                <img src=\"".concat(serie.urlImagen, "\" class=\"card-img-top\" alt=\"").concat(serie.nombre, "\">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title\">").concat(serie.nombre, "</h5>\n                                    <p class=\"card-text\">").concat(serie.descripcion, "</p>\n                                    <a href=\"").concat(serie.url, "\" target=\"_blank\">").concat(serie.url, "</a>\n                                </div>\n                            </div>");
 }
